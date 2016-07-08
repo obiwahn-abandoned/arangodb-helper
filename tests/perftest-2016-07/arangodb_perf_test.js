d7fa3acb19f43075ed20f5488c8c4a4a2cb9c57e
Jan Christoph Uhde <Jan@UhdeJC.com>
2016-07-08 21:14:14 +0200

    update javascript for testing

--- tests/perftest-2016-07/arangodb_perf_test.js
+++ tests/perftest-2016-07/arangodb_perf_test.js
@@ -2,10 +2,15 @@
 var fs = require('fs')
 
 function load_data(directory, col){
-    print("get files")
+    print("get files in: " + directory  )
     var files = fs.list(directory)
+    if (files.length == 0){
+        print("no files to load");
+        col = null;
+        return col;
+    }
     files.sort()
-
+    print("OBI" + col)
     var buffer;
     var uid = 0;
     files.forEach( function(file){
@@ -30,26 +35,68 @@ function get_collection(name, drop){
         db._drop(name)
     }
     var col = db[name];
-    if (col) { return col; }
+    if (col) {
+        print("returning existing collection")
+        col.load()
+        return col;
+    }
     print("create new")
-    return db._create(name)
+    col = db._create(name)
+    col = load_data('/home/arango/perf-test-obi/testdata-10', col)
+    return col
 }
 
 function run_test(col){
-    //ask for max key
-    //random access key
-    //replace randomenss later with a fixed list of keys
-    //do timing
+    print("starting test")
+    var time = require('internal').time
+    var start = time()
 
+    // //very slow
+    // var biggest_key = function(col){
+    //     db._query(
+    //     ` FOR doc IN @@col
+    //         let key = to_number(doc._key)
+    //         SORT key
+    //         LIMIT 1
+    //         RETURN key
+    //     `, { "@col" : col.name() }
+    //     )
+    // }(col)
+
+
+    print(col.properties());
+    var count = col.count().toString();
+    print("there are " + count + " elements in the collection");
+
+    function next_element_all(current, count, prim){
+        return (( current + prim ) % count)
+    }
+
+    var prim = Math.pow(2,31) - 1;
+    print("prime: " + prim.toString());
+    var key = 1;
+
+    var test_count = count / 10;
+    print(test_count)
+    for(var i = 0; i < test_count/10; ++i){
+        col.document(key.toString())
+        key = next_element_all(key,count,prim);
+    }
+
+    return (time() - start)
 }
 
 function main(){
-    const drop_db = false
-    var col = get_collection("obi-perf-test",drop_db)
-    if(drop_db){
-        load_data('/home/arango/testdata', col)
+    var col = get_collection("obi-perf-test-10", true)
+    if(!col){
+        print("no collection")
+        return 1
     }
-    var time = run_test(col)
+    var duration = run_test(col)
+
+    print("total time taken: " + duration.toString())
+    //print(col.figures())
+    return 0
 }
 
 main()
