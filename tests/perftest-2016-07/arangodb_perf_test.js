b34c76a4ca1563e75c0f2f7a02859ca32b25cbaf
Jan Christoph Uhde <Jan@UhdeJC.com>
2016-07-12 22:46:37 +0200

    small fixes

--- tests/perftest-2016-07/arangodb_perf_test.js
+++ tests/perftest-2016-07/arangodb_perf_test.js
@@ -2,6 +2,7 @@
     'use strict'
     var fs = require('fs')
     var db = require('internal').db
+    var time = require('internal').time
 
     function load_data(directory, col){
         print("get files in: " + directory  )
@@ -39,7 +40,10 @@
         var col = db[name];
         if (col) {
             print("returning existing collection")
+	    print("loading collection")
+            var start = time()
             col.load()
+            print(" loading took " + (time() - start).toString() + " seconds")
             return col;
         }
         print("create new")
@@ -50,7 +54,6 @@
 
     function run_test(col){
         print("starting test")
-        var time = require('internal').time
         var start = time()
 
         // //very slow
@@ -67,22 +70,45 @@
 
 
         print(col.properties());
-        var count = col.count().toString();
-        print("there are " + count + " elements in the collection");
+        var count = col.count();
+        print("there are " + count.toString() + " elements in the collection");
 
-        function next_element_all(current, count, prim){
-            return (( current + prim ) % count)
+        function next_key_max_all(current, count){
+            return (( current + 47 ) % count)
+        }
+        
+	function next_key_max_1(current, count){
+            return (( current + 47 * 100 ) % count)
         }
 
-        var prim = Math.pow(2,31) - 1;
-        print("prime: " + prim.toString());
+	function next_key_lcg(current){
+		//https://en.wikipedia.org/wiki/Linear_congruential_generator
+		return (48271 * current % 2147483647);
+	}
+
+	function create_yield_next(initial, count){
+		var current = initial;
+		return (function(){
+			current = next_key_lcg(current) % count;
+			return current % count;
+		})
+	}
+
+	var yield_next = create_yield_next(1,count)
         var key = 1;
 
         var test_count = count / 10;
         print(test_count)
-        for(var i = 0; i < test_count/10; ++i){
-            col.document(key.toString())
-            key = next_element_all(key,count,prim);
+        for(var i = 0; i < test_count; ++i){
+            var doc = col.document(key.toString())
+	    if(i < 10){
+		print( key.toString() + " -- " + doc["data"] );
+	    }
+	    if (i % 100000 == 0){print(i)}
+            key = next_key_max_1(key,count);
+	    //print(key);
+	    //key = yield_next()
+	    
         }
 
         return (time() - start)
@@ -93,11 +119,12 @@
         if(!col){
             print("no collection")
             return 1
-        }
+	}
+        
         var duration = run_test(col)
 
         print("total time taken: " + duration.toString())
-        //print(col.figures())
+        print(col.figures())
         return 0
     }
 
