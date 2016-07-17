8021481870d54e6b614a2a719bc01033246faedd
Jan Christoph Uhde <Jan@UhdeJC.com>
2016-07-17 21:58:02 +0200

    fix

--- tests/perftest-2016-07/cpp_test/src/wiredtiger.cpp
+++ tests/perftest-2016-07/cpp_test/src/wiredtiger.cpp
@@ -24,13 +24,25 @@ WT_CURSOR *cursor;
 WT_SESSION *session;
 const char *key, *value;
 
+
+int
+get_stat(WT_CURSOR *cursor, int stat_field, uint64_t *valuep)
+{
+        const char *desc, *pvalue;
+        int ret;
+        cursor->set_key(cursor, stat_field);
+        if ((ret = cursor->search(cursor)) != 0)
+                return (ret);
+        return (cursor->get_value(cursor, &desc, &pvalue, valuep));
+}
+
 static std::size_t
 run_test(std::size_t(*next_key)(std::size_t, std::size_t)
         ,std::size_t count
         ){
 
     auto start = cclock::now();
-    auto test_count = count / 10;
+    std::size_t test_count = count; // / 10;
     int ret = 0;
 
     std::string data;
@@ -50,15 +62,12 @@ run_test(std::size_t(*next_key)(std::size_t, std::size_t)
             if (ret != 0){
                 std::cout << " could not get value" << std::endl ;
             }
-            if (i%1000000 == 0){
+            if (i % 1000000 == 0){
                 std::cout << key << " -- " << value << std::endl;
             }
             if (i < 10){
                     std::cout << key << " -- " << value << std::endl;
             }
-        }
-        if (i%10000 == 0){
-            std::cout << " " << i << " ";
         }
         ret = cursor->reset(cursor);
         key = next_key(key, count);
@@ -83,7 +92,7 @@ int run_tiger(options opts)
     home = opts.db_directory.c_str();
 
 	/* Open a connection to the database, creating it if necessary. */
-	if ((ret = wiredtiger_open(home, NULL, "create", &conn)) != 0 ||
+	if ((ret = wiredtiger_open(home, NULL, "create,cache_size=25GB", &conn)) != 0 ||
 	    (ret = conn->open_session(conn, NULL, NULL, &session)) != 0) {
 		fprintf(stderr, "Error connecting to %s: %s\n",
 		    home, wiredtiger_strerror(ret));
@@ -148,7 +157,7 @@ int run_tiger(options opts)
 
                 check_exit_on_fail(ret);
 
-                if (index % 100000 == 0){
+                if (index % 1000000 == 0){
                     ret = session->commit_transaction(session, NULL);
                     check_exit_on_fail(ret);
                     ret = session->begin_transaction(session, NULL);
@@ -170,12 +179,18 @@ int run_tiger(options opts)
 	ret = session->open_cursor(session, table.c_str(), NULL, NULL, &cursor);
     check_exit_on_fail(ret);
 
+    std::cout << "get statistics " << std::endl;
+    uint64_t number_of_key_value_pairs = 300 * 1000 * 1000;
+    //ret = get_stat(cursor, WT_STAT_DSRC_BTREE_ENTRIES, &number_of_key_value_pairs);
+    check_exit_on_fail(ret);
+
+    std::cout << "number of entries in table: " << number_of_key_value_pairs << std::endl;
     if (opts.test.compare("all") != 0){
-    std::cout << "algorithm:" << opts.test << std::endl;
-    std::cout << "first  run warum up: " << std::endl
-              << "seconds taken: " << run_test(get_next_key_algorithm(opts.test) , 300*1000*1000) << std::endl;
-    std::cout << "second - real test:  " << std::endl
-              << "seconds taken: " << run_test(get_next_key_algorithm(opts.test) , 300*1000*1000) << std::endl;
+        std::cout << "algorithm:" << opts.test << std::endl;
+        std::cout << "first  run warum up: " << std::endl
+                  << "seconds taken: " << run_test(get_next_key_algorithm(opts.test), number_of_key_value_pairs) << std::endl;
+        std::cout << "second - real test:  " << std::endl
+                  << "seconds taken: " << run_test(get_next_key_algorithm(opts.test), number_of_key_value_pairs) << std::endl;
     }
 
     ret = session->close(session, NULL);
